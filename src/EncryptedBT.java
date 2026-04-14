2899d92fe57563c757dd29e06c53d19b1bfc0943
jojamullamudi <jose.jamul2005@gmail.com>
2026-04-14 09:54:24 -0500

    started q4 and added the inital method to find the element

---
+++ src/EncryptedBT.java
@@ -0,0 +1,25 @@
+public class EncryptedBT {
+
+
+    public boolean findElement( int[] tree, int element)
+    {
+        if( tree == null)
+        {
+            return false;
+        }
+        if ( tree.length == 0)
+        {
+            return false;
+        }
+        if ( tree[0] == -1)
+        {
+            return false;
+        }
+
+        return checkVal( )
+    }
+
+    public boolean checkVal
+
+
+}
