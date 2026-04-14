77dd5c6df957bf64de56305919be38ae5762d27f
jojamullamudi <jose.jamul2005@gmail.com>
2026-04-13 22:58:34 -0500

    Initial commit

---
+++ src/Huffman.java
@@ -0,0 +1,27 @@
+import java.util.Hashtable;
+
+public class Huffman {
+
+    public Hashtable<Character, Integer> counts;
+
+    public Hashtable<Character, Integer> frequencyCount(String input) {
+        Hashtable<Character, Integer> counts = new Hashtable<>();
+        for (int a = 0; a < input.length(); a++)
+        {
+            if (counts.containsKey(input.charAt(a)))
+            {
+                counts.replace(input.charAt(a), (int)counts.get(input.charAt(a)) + 1);
+            }
+            lse
+            {
+                counts.put(input.charAt(a), 1);
+            }
+        }
+        return counts;
+    }
+    
+
+
+
+
+}
