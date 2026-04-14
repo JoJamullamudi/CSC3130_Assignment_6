ba3fc2b4f8e1294b071e78da116664d9f1b3e6b0
jojamullamudi <jose.jamul2005@gmail.com>
2026-04-14 10:04:20 -0500

    made the checkVal method which checks if the current index is valid then compares. if not true then runs the method again with the left and right child

--- src/EncryptedBT.java
+++ src/EncryptedBT.java
@@ -19,7 +19,30 @@ public class EncryptedBT {
         return checkVal( )
     }
 
-    public boolean checkVal
+    public boolean checkVal( int[] tree, int element, int index, int val)
+    {
+        if (index >= tree.length) // checks to see if the current index is in bounds
+        {
+            return false;
+        }
+        if (tree[index] == -1) // checks to see if current node is empty
+        {
+            return false;
+        }
+
+        if ( val == element)// checks if the value is the element we're looking for
+        {
+            return true;
+        }
+
+        int left = 3*val + 1; // makes left child value
+        int right = 2*val + 5; // makes right child value
+
+        checkVal(tree, element, index+1, left); // runs check again with the left child
+        checkVal(tree, element, index+2, right);// runs check again with the right child
+
+
+    }
 
 
 }
